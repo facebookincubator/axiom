9f441c2501dfa3e6cbc0487e332b10a557a245ae
mbkkt <mbkkt@users.noreply.github.com>
2025-11-06 16:56:11 +0100

    fix: Translation of Joins into query graph
    Fix translation of Joins to DTs. Join(Filter(A), Filter(B)) used to be translated into a single DT, but now it becomes 3 DTs:
        DT for Filter(A)
        DT for Filter(B)
        DT with the join.
    
    Extra wrapping fixes cases where A or B are DTs themselves (see example in #557). The cases where A and B are BaseTables end up with a single DT like before thanks to pre-existing flattenDt logic. In these cases, we first produce 3 DTs, then flatten these into one.
    
    When translating a join, left side is wrapped in a new DT if it contains any of Unnest, Aggregate, Limit, Filter, Sort. Same applies to the right side. For non-inner joins, right side is additionally wrapped in a new DT if it contains another Join.
    
    There was important part of how joins are handled (in case when left part of join tree contained non-deterministic filter).
    This code was simplified just to separate DT for filter input + filter
    
    Part of #536 (dropping unnecessary order by is still TBD)
    Fixes #557

---
+++ axiom/optimizer/tests/JoinTest.cpp
@@ -0,0 +1,160 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "axiom/connectors/tests/TestConnector.h"
+#include "axiom/logical_plan/PlanBuilder.h"
+#include "axiom/optimizer/tests/HiveQueriesTestBase.h"
+#include "axiom/optimizer/tests/PlanMatcher.h"
+#include "axiom/optimizer/tests/QueryTestBase.h"
+#include "velox/exec/tests/utils/PlanBuilder.h"
+
+namespace facebook::axiom::optimizer::test {
+namespace {
+
+using namespace velox;
+namespace lp = facebook::axiom::logical_plan;
+
+class JoinTest : public QueryTestBase {
+ protected:
+  static constexpr auto kTestConnectorId = "test";
+
+  void SetUp() override {
+    QueryTestBase::SetUp();
+
+    testConnector_ =
+        std::make_shared<connector::TestConnector>(kTestConnectorId);
+    velox::connector::registerConnector(testConnector_);
+  }
+
+  void TearDown() override {
+    velox::connector::unregisterConnector(kTestConnectorId);
+
+    QueryTestBase::TearDown();
+  }
+
+  std::shared_ptr<connector::TestConnector> testConnector_;
+};
+
+TEST_F(JoinTest, joinWithFilterOverLimit) {
+  testConnector_->addTable("t", ROW({"a", "b", "c"}, BIGINT()));
+  testConnector_->addTable("u", ROW({"x", "y", "z"}, BIGINT()));
+
+  lp::PlanBuilder::Context ctx(kTestConnectorId);
+  auto logicalPlan =
+      lp::PlanBuilder(ctx)
+          .tableScan("t")
+          .limit(100)
+          .filter("b > 50")
+          .join(
+              lp::PlanBuilder(ctx).tableScan("u").limit(50).filter("y < 100"),
+              "a = x",
+              lp::JoinType::kInner)
+          .build();
+
+  {
+    auto plan = toSingleNodePlan(logicalPlan);
+    auto matcher = core::PlanMatcherBuilder()
+                       .tableScan("t")
+                       .limit()
+                       .filter("b > 50")
+                       .hashJoin(
+                           core::PlanMatcherBuilder()
+                               .tableScan("u")
+                               .limit()
+                               .filter("y < 100")
+                               .build())
+                       .build();
+
+    AXIOM_ASSERT_PLAN(plan, matcher);
+  }
+}
+
+TEST_F(JoinTest, outerJoinWithInnerJoin) {
+  testConnector_->addTable("t", ROW({"a", "b", "c"}, BIGINT()));
+  testConnector_->addTable("v", ROW({"vx", "vy", "vz"}, BIGINT()));
+  testConnector_->addTable("u", ROW({"x", "y", "z"}, BIGINT()));
+
+  lp::PlanBuilder::Context ctx(kTestConnectorId);
+  auto logicalPlan = lp::PlanBuilder(ctx)
+                         .tableScan("t")
+                         .filter("b > 50")
+                         .join(
+                             lp::PlanBuilder(ctx).tableScan("u").join(
+                                 lp::PlanBuilder(ctx).tableScan("v"),
+                                 "x = vx",
+                                 lp::JoinType::kInner),
+                             "a = x",
+                             lp::JoinType::kLeft)
+                         .build();
+
+  {
+    SCOPED_TRACE("left join with inner join on right");
+
+    auto plan = toSingleNodePlan(logicalPlan);
+    auto matcher =
+        core::PlanMatcherBuilder()
+            .tableScan("t")
+            .filter("b > 50")
+            .hashJoin(
+                core::PlanMatcherBuilder()
+                    .tableScan("u")
+                    .hashJoin(core::PlanMatcherBuilder().tableScan("v").build())
+
+                    .build())
+            .build();
+
+    AXIOM_ASSERT_PLAN(plan, matcher);
+  }
+
+  logicalPlan = lp::PlanBuilder(ctx)
+                    .tableScan("t")
+                    .filter("b > 50")
+                    .aggregate({"a", "b"}, {"sum(c)"})
+                    .join(
+                        lp::PlanBuilder(ctx)
+                            .tableScan("u")
+                            .join(
+                                lp::PlanBuilder(ctx).tableScan("v"),
+                                "x = vx",
+                                lp::JoinType::kInner)
+                            .filter("not(x = vy)"),
+                        "a = x",
+                        lp::JoinType::kLeft)
+                    .build();
+
+  {
+    SCOPED_TRACE("aggregation left join filter over inner join");
+    auto plan = toSingleNodePlan(logicalPlan);
+    auto matcher =
+        core::PlanMatcherBuilder()
+            .tableScan("t")
+            .filter()
+            .aggregation()
+            .hashJoin(
+                core::PlanMatcherBuilder()
+                    .tableScan("u")
+                    .hashJoin(core::PlanMatcherBuilder().tableScan("v").build())
+                    .filter()
+                    .build())
+            .project()
+            .build();
+
+    AXIOM_ASSERT_PLAN(plan, matcher);
+  }
+}
+
+} // namespace
+} // namespace facebook::axiom::optimizer::test
