774c40cf455bfc85f18aca91f9f9afa5c516ab4d
Masha Basmanova <mbasmanova@meta.com>
2026-01-12 16:07:11 -0800

    refactor: Extract ExprResolver class into its own header (#747)
    Summary: Pull Request resolved: https://github.com/facebookincubator/axiom/pull/747
    
    Reviewed By: xiaoxmeng
    
    Differential Revision: D90533466
    
    fbshipit-source-id: 7a8d57c28b5b382cb52e2574da961805bc44693d

--- axiom/logical_plan/PlanBuilder.h
+++ axiom/logical_plan/PlanBuilder.h
@@ -17,85 +17,16 @@
 #pragma once
 
 #include "axiom/logical_plan/ExprApi.h"
+#include "axiom/logical_plan/ExprResolver.h"
 #include "axiom/logical_plan/LogicalPlanNode.h"
 #include "axiom/logical_plan/NameAllocator.h"
-#include "velox/core/ITypedExpr.h"
 #include "velox/core/QueryCtx.h"
-#include "velox/parse/Expressions.h"
 #include "velox/parse/ExpressionsParser.h"
 #include "velox/parse/PlanNodeIdGenerator.h"
 
 namespace facebook::axiom::logical_plan {
 
 class NameMappings;
-
-/// Class encapsulating functions for type inference and constant folding. Use
-/// with SQL and PlanBuilder.
-class ExprResolver {
- public:
-  using InputNameResolver = std::function<ExprPtr(
-      const std::optional<std::string>& alias,
-      const std::string& fieldName)>;
-
-  /// Maps from an untyped call and  resolved arguments to a resolved function
-  /// call. Use only for anamolous functions where the type depends on constant
-  /// arguments, e.g. Koski make_row_from_map().
-  using FunctionRewriteHook = std::function<
-      ExprPtr(const std::string& name, const std::vector<ExprPtr>& args)>;
-
-  ExprResolver(
-      std::shared_ptr<velox::core::QueryCtx> queryCtx,
-      bool enableCoercions,
-      FunctionRewriteHook hook = nullptr,
-      std::shared_ptr<velox::memory::MemoryPool> pool = nullptr)
-      : queryCtx_(std::move(queryCtx)),
-        enableCoercions_{enableCoercions},
-        hook_(std::move(hook)),
-        pool_(std::move(pool)) {}
-
-  ExprPtr resolveScalarTypes(
-      const velox::core::ExprPtr& expr,
-      const InputNameResolver& inputNameResolver) const;
-
-  AggregateExprPtr resolveAggregateTypes(
-      const velox::core::ExprPtr& expr,
-      const InputNameResolver& inputNameResolver,
-      const ExprPtr& filter,
-      const std::vector<SortingField>& ordering,
-      bool distinct) const;
-
- private:
-  ExprPtr resolveLambdaExpr(
-      const velox::core::LambdaExpr* lambdaExpr,
-      const std::vector<velox::TypePtr>& lambdaInputTypes,
-      const InputNameResolver& inputNameResolver) const;
-
-  ExprPtr tryResolveCallWithLambdas(
-      const std::shared_ptr<const velox::core::CallExpr>& callExpr,
-      const InputNameResolver& inputNameResolver) const;
-
-  ExprPtr tryFoldCall(
-      const velox::TypePtr& type,
-      const std::string& name,
-      const std::vector<ExprPtr>& inputs) const;
-
-  ExprPtr tryFoldCast(const velox::TypePtr& type, const ExprPtr& input) const;
-
-  velox::core::TypedExprPtr makeConstantTypedExpr(const ExprPtr& expr) const;
-
-  ExprPtr makeConstant(const velox::VectorPtr& vector) const;
-
-  ExprPtr tryFoldCall(const velox::TypePtr& type, ExprPtr input) const;
-
-  ExprPtr tryFoldSpecialForm(
-      const std::string& name,
-      const std::vector<ExprPtr>& inputs) const;
-
-  std::shared_ptr<velox::core::QueryCtx> queryCtx_;
-  const bool enableCoercions_;
-  FunctionRewriteHook hook_;
-  std::shared_ptr<velox::memory::MemoryPool> pool_;
-};
 
 // Make sure to specify Context.queryCtx to enable constand folding.
 class PlanBuilder {
